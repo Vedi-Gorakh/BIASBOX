c09cd004efe4fb9224f0b95bbdc19c003747e5d1
Vedi Gorakh <vedigorakh303@gmail.com>
2025-09-28 14:54:43 +0530

    Fix CORS origin and API endpoint

--- src/components/utils/axiosInstance.js
+++ src/components/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://biasbox.onrender.com', // Render backend URL
+  baseURL: 'https://biasbox.onrender.com/api', // Render backend URL
 });
 
 axiosInstance.interceptors.request.use((config) => {
